59e8150048d9947de3cd3a824f5a3c83641db126
wenxiangmin <wen17375792092>
2022-06-17 10:53:49 +0800

    init

---
+++ src/main/java/test/Application.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wenxiangmin
+ * @ClassName Application.java
+ * @Description TODO
+ * @createTime 2022年06月17日 10:50:00
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
---
+++ src/main/java/test/TestController.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wenxiangmin
+ * @ClassName TestController.java
+ * @Description TODO
+ * @createTime 2022年06月17日 10:48:00
+ */
+@RestController
+@RequestMapping("test")
+public class TestController {
+    @GetMapping("t1")
+    public String test1() {
+        return "t1";
+    }
+}
